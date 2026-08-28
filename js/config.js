1f63b1b611b93e144ee3c13c923eaf1c976ea7ba
aidoru67 <lasolita@gmail.com>
2026-08-28 17:59:08 +0200

    Delete js directory

--- js/config.js
+++
@@ -1,6 +0,0 @@
-export const CONFIG = Object.freeze({
-  supabaseUrl: 'https://amehnlagrydfilqhpdtk.supabase.co',
-  supabaseAnonKey: 'sb_publishable_c3RMIc7jFuNvwqvEygL7AQ_ny1S4IpG',
-  filmsTable: 'films',
-  batchSize: 10
-});
